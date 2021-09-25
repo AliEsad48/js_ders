f1f3a95dbd5b2a74635589980f651ac6ab91fc04
Salih Bayram <alicamci2020@gmail.com>
2021-09-25 17:14:18 +0300

    V7

--- ders4_dom/ornek1_selector/app.js
+++ ders4_dom/ornek1_selector/app.js
@@ -5,3 +5,9 @@ const paragraf = document.getElementById("para").style;
 paragraf.backgroundColor ="black";
 paragraf.color="white";
 paragraf.fontSize="20px";
+
+
+const buton = document.getElementById("btn");
+buton.style.width="100px";
+buton.style.color="red";
+buton.style.fontSize="18px"
