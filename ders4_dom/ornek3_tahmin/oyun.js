86297cc58388286cf8a2bd6eabc7d896c29244d5
Salih Bayram <alicamci2020@gmail.com>
2021-09-28 22:12:09 +0300

    V20

--- ders4_dom/ornek3_tahmin/oyun.js
+++ ders4_dom/ornek3_tahmin/oyun.js
@@ -1,4 +1,46 @@
+//bilgisayar 1-20 arasında bir sayı tut
+
+ let rastgeleSayi=   Math.floor(Math.random()*20+1);
+let puanPc=10;
+ let rekorPc=0;
+ 
+ //her kontrol et butonuna tıklandığında çalıştırdım
+ document.querySelector(".kontrol").onclick=function()
+ {
+   //tahminim doğruysa
+   const tahmin=document.querySelector(".tahmin").value;
+   if(tahmin==rastgeleSayi){
+document.querySelector("body").style.backgroundColor="green";
+document.querySelector(".question").textContent=rastgeleSayi;
+document.querySelector(".mesaj").textContent="🎉Tebrikler Bildiniz";
+
+//rekoru güncelle
+if (puanPc>rekorPc) {
+
+    rekorPc=puanPc;
+    document.querySelector(".rekor-skor").textContent=puanPc;
+    
+}
+
+   }
+
+   else{
+       if (puanPc>1) {
+        puanPc--;  
+        
+        tahmin<rastgeleSayi ? (document.querySelector(".mesaj").textContent= "📈Arttır"):(document.querySelector(".mesaj").textContent="📉Azalt");
+        document.querySelector(".skor").textContent=puanPc;
+       }
+   }
+
+ };
 
 
 
-Math.floor(Math.random()*20+1)
+
+
+
+
+
+
+
